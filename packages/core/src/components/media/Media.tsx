4bd8e55df1cf45fb82cf8ff5068f3f7e8a74010d
Adarsh Pastakia <adarshpastakia@outlook.com>
2025-03-22 00:15:12 +0400

    fix(Media): missing media fallback icon

--- packages/core/src/components/media/Media.tsx
+++ packages/core/src/components/media/Media.tsx
@@ -23,10 +23,11 @@
 
 import { getImageColorset } from "@react-fabric/utilities";
 import classNames from "classnames";
-import { useMemo, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { type AriaProps, type CssProp, type TestProps } from "../../types";
 import { CoreIcons } from "../../types/icons";
 import { Button } from "../button/Button";
+import { Icon } from "../icon/Icon";
 
 export interface BaseProps extends CssProp, TestProps, AriaProps {
   /**
@@ -70,6 +71,17 @@ export interface VideoProps extends BaseProps {
   playOnHover?: boolean;
 }
 
+const loadIndicator = (
+  <div
+    className={classNames(
+      "fabric-mediaPlaceholder",
+      "absolute z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl text-gray animate-pulse",
+    )}
+  >
+    ●●●
+  </div>
+);
+
 export const Image = ({
   src,
   alt,
@@ -79,19 +91,12 @@ export const Image = ({
   ...aria
 }: ImageProps) => {
   const [colorSet, setColorSet] = useState("light");
-  const loadIndicator = useMemo(
-    () => (
-      <div
-        className={classNames(
-          "fabric-mediaPlaceholder",
-          "absolute z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl text-gray animate-pulse",
-        )}
-      >
-        ●●●
-      </div>
-    ),
-    [src],
-  );
+  const [loaded, setLoaded] = useState(false);
+  const [errored, setErrored] = useState(false);
+  useEffect(() => {
+    setLoaded(false);
+    setErrored(false);
+  }, [src]);
   return (
     <div
       data-ref="image"
@@ -104,22 +109,32 @@ export const Image = ({
       data-colorset={colorSet}
       {...aria}
     >
-      {loadIndicator}
-      <img
-        alt={alt ?? src}
-        src={src}
-        loading="lazy"
-        crossOrigin="anonymous"
-        className={classNames(
-          "fabric-media",
-          "pointer-events-none w-full h-full",
-          cover[fit],
-        )}
-        onLoad={(e) => {
-          e.currentTarget.previousElementSibling?.remove();
-          setColorSet(getImageColorset(e.currentTarget));
-        }}
-      />
+      {!loaded && !errored && loadIndicator}
+      {!errored && (
+        <img
+          alt={alt ?? src}
+          src={src}
+          loading="lazy"
+          crossOrigin="anonymous"
+          className={classNames(
+            "fabric-media",
+            "pointer-events-none w-full h-full",
+            cover[fit],
+          )}
+          onLoad={(e) => {
+            setLoaded(true);
+            setColorSet(getImageColorset(e.currentTarget));
+          }}
+          onError={() => setErrored(true)}
+        />
+      )}
+      {errored && (
+        <Icon
+          size="2rem"
+          className="absolute text-tint-300 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          icon={CoreIcons.mediaImageBroken}
+        />
+      )}
     </div>
   );
 };
@@ -135,9 +150,19 @@ export const Video = ({
   ...aria
 }: VideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [loaded, setLoaded] = useState(false);
+  const [errored, setErrored] = useState(false);
+  useEffect(() => {
+    setLoaded(false);
+    setErrored(false);
+  }, [src]);
 
   const [state, setState] = useState(true);
   const [volume, setVolume] = useState(false);
+
+  useEffect(() => {
+    setErrored(false);
+  }, [src]);
 
   return (
     <div
@@ -156,74 +181,79 @@ export const Video = ({
       }}
       {...aria}
     >
-      <div
-        className={classNames(
-          "fabric-mediaPlaceholder",
-          "absolute z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl animate-pulse",
-        )}
-      >
-        ●●●
-      </div>
-      <video
-        loop
-        ref={videoRef}
-        controls={false}
-        poster={poster}
-        autoPlay={autoPlay}
-        className={classNames(
-          "fabric-media",
-          "pointer-events-none w-full h-full",
-          fit && `object-${fit}`,
-        )}
-        onPlay={() => setState(true)}
-        onPause={() => setState(false)}
-        onVolumeChange={(e) => setVolume(!!e.currentTarget.volume)}
-        onLoadedData={(e) => {
-          e.currentTarget.volume = 0;
-          e.currentTarget.previousElementSibling?.remove();
-        }}
-      >
-        <source src={src} />
-        <track kind="captions" />
-      </video>
-      <div
-        data-inner-clickable
-        className={classNames(
-          "fabric-mediaControls",
-          "absolute bottom-6 end-6 rounded-full flex flex-col",
-        )}
-      >
-        <Button
-          rounded
-          variant="link"
-          className={"fabric-mediaControlPlaceholder"}
-          icon={CoreIcons.ellipsis}
-          aria-label="controls"
+      {!loaded && !errored && loadIndicator}
+      {!errored && (
+        <Fragment>
+          <video
+            loop
+            ref={videoRef}
+            controls={false}
+            poster={poster}
+            autoPlay={autoPlay}
+            className={classNames(
+              "fabric-media",
+              "pointer-events-none w-full h-full",
+              fit && `object-${fit}`,
+            )}
+            onPlay={() => setState(true)}
+            onPause={() => setState(false)}
+            onVolumeChange={(e) => setVolume(!!e.currentTarget.volume)}
+            onLoadedData={(e) => {
+              e.currentTarget.volume = 0;
+              setLoaded(true);
+            }}
+            onError={() => setErrored(true)}
+          >
+            <source src={src} />
+            <track kind="captions" />
+          </video>
+          <div
+            data-inner-clickable
+            className={classNames(
+              "fabric-mediaControls",
+              "absolute bottom-6 end-6 rounded-full flex flex-col",
+            )}
+          >
+            <Button
+              rounded
+              variant="link"
+              className={"fabric-mediaControlPlaceholder"}
+              icon={CoreIcons.ellipsis}
+              aria-label="controls"
+            />
+            <Button
+              rounded
+              variant="link"
+              aria-label="controls"
+              className={"fabric-mediaControlAction"}
+              onClick={() => {
+                videoRef.current &&
+                  (videoRef.current.volume = videoRef.current.volume ? 0 : 1);
+              }}
+              icon={volume ? CoreIcons.volumeOn : CoreIcons.volumeOff}
+            />
+            <Button
+              rounded
+              variant="link"
+              aria-label="controls"
+              className={"fabric-mediaControlAction"}
+              onClick={() => {
+                videoRef.current?.paused
+                  ? videoRef.current?.play()
+                  : videoRef.current?.pause();
+              }}
+              icon={state ? CoreIcons.pause : CoreIcons.play}
+            />
+          </div>
+        </Fragment>
+      )}
+      {errored && (
+        <Icon
+          size="2rem"
+          className="absolute text-tint-300 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          icon={CoreIcons.mediaVideoBroken}
         />
-        <Button
-          rounded
-          variant="link"
-          aria-label="controls"
-          className={"fabric-mediaControlAction"}
-          onClick={() => {
-            videoRef.current &&
-              (videoRef.current.volume = videoRef.current.volume ? 0 : 1);
-          }}
-          icon={volume ? CoreIcons.volumeOn : CoreIcons.volumeOff}
-        />
-        <Button
-          rounded
-          variant="link"
-          aria-label="controls"
-          className={"fabric-mediaControlAction"}
-          onClick={() => {
-            videoRef.current?.paused
-              ? videoRef.current?.play()
-              : videoRef.current?.pause();
-          }}
-          icon={state ? CoreIcons.pause : CoreIcons.play}
-        />
-      </div>
+      )}
     </div>
   );
 };
