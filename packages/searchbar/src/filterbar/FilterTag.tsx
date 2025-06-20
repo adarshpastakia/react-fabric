cba3f32d293b084967565f042519aa68c19debcf
Adarsh Pastakia <adarshpastakia@outlook.com>
2025-06-20 14:35:54 +0400

    fix(FilterTag): wrap label in a div with truncate class for better overflow handling

--- packages/searchbar/src/filterbar/FilterTag.tsx
+++ packages/searchbar/src/filterbar/FilterTag.tsx
@@ -118,7 +118,7 @@ export const FilterTag = ({
               icon={filter.icon ?? CoreIcons.pin}
             />
           )}
-          {label}
+          <div className="truncate">{label}</div>
         </div>
       </Chip>
       <FilterMenu
