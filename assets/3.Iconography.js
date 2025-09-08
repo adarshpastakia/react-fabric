c1896cc6fb7cae9440b4e82a862eaddaa66d4d40
adarshpastakia <adarshpastakia@outlook.com>
2025-09-08 18:23:00 +0000

    Deploying to gh-pages from @ adarshpastakia/react-fabric@a285cb461ffada33c94d7c7bd5da04c7312f3392 🚀

--- assets/3.Iconography.js
+++ assets/3.Iconography.js
@@ -1,3 +1,3 @@
-import{j as n,M as a,T as s,aZ as c,a6 as i,a_ as x,a5 as l}from"./iframe-6XIGnDH8.js";import{useMDXComponents as j}from"./index.js";function e(t){return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Iconography"}),`
+import{j as n,M as r,T as s,aZ as c,a6 as i,a_ as l,a5 as x}from"./iframe-6XIGnDH8.js";import{useMDXComponents as m}from"./index.js";function t(o){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"Iconography"}),`
 `,n.jsx(s,{children:"Iconography"}),`
-`,n.jsx(c,{children:Object.entries(i).sort().map(([o,r])=>n.jsx(x,{name:o,children:n.jsx(l,{icon:r,onClick:()=>{navigator.clipboard.writeText(`CoreIcons.${o}`)}})},o))})]})}function d(t={}){const{wrapper:o}={...j(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e()}export{d as default};
+`,n.jsx(c,{children:Object.entries(i).sort().map(([e,a])=>n.jsx(l,{name:e,children:n.jsx(x,{icon:a,className:"p-2",onClick:()=>{navigator.clipboard.writeText(`CoreIcons.${e}`)}})},e))})]})}function d(o={}){const{wrapper:e}={...m(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(t,{...o})}):t()}export{d as default};
