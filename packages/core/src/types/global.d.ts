d4e623cc47982ec59363f0bcbc7cca29f3f90a15
Adarsh Pastakia <adarshpastakia@outlook.com>
2026-04-08 10:05:27 +0400

    ops: updated global typing

--- packages/core/src/types/global.d.ts
+++ packages/core/src/types/global.d.ts
@@ -53,6 +53,10 @@ declare module "*.jpeg" {
   const content: any;
   export default content;
 }
+declare module "*.webp" {
+  const content: any;
+  export default content;
+}
 declare module "*.svg" {
   const content: any;
   export default content;
